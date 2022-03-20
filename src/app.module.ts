8ea78a00545e4f3eb645f4688cc80fc9154a76ba
Luis Borochok <luis.borochok@gmail.com>
2022-03-20 12:52:53 -0300

    ci: set docker config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env', '.env.dev'],
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) =>
+        ({
+          type: 'postgres' as const,
+          port: +configService.get<number>('POSTGRES_PORT', 5432),
+          host: configService.get<string>('POSTGRES_URL', 'postgres'),
+          username: configService.get<string>('POSTGRES_USER', 'postgres'),
+          password: configService.get<string>('POSTGRES_PASSWORD', 'admin'),
+          database: configService.get<string>('POSTGRES_DB', 'core-bank'),
+          // autoLoadEntities: true,
+          // connectTimeoutMS: 4000,
+        } as PostgresConnectionOptions),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('bootstrap');
+
   await app.listen(3000);
+  logger.log(`Server start at port ${3000}`);
 }
 bootstrap();
